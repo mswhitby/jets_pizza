2d645870b692276160164d756532bf78dced17c1
deandrawhitby <104539455+deandrawhitby@users.noreply.github.com>
2023-01-26 08:57:58 -0600

    Update period_1.py

--- period_1.py
+++ period_1.py
@@ -1,8 +1,16 @@
 def price_pizza(toppings):
     
+    # print(toppings)
+    toppings = toppings.upper()
+    # print(toppings)
+    
     base_price = 15
+    calculated_toppings_list = []
     
     for letter in toppings:
+        
+        if letter in calculated_toppings_list:
+            continue
         
         if letter == "T":
             base_price = base_price + 1.5
@@ -20,17 +28,19 @@ def price_pizza(toppings):
             base_price = base_price + .40
             
         print(letter, base_price)
+        calculated_toppings_list.append(letter)
             
     if base_price > 20:
         base_price = base_price - (base_price *.05) 
         # base_price = base_price * .95
             
             
-    return base_price
+    return round(base_price, 2)
     
     
 if __name__ == "__main__":
-    print(price_pizza("AAAAAAAMMTGTMMMXMMT"))
+    # print(price_pizza("AAAAAAAMMTGTMMMXMMT"))
+    print(price_pizza("aaaaaammtgtmmmxmmt"))
 
     
 
