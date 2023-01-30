4172070bac2d43135cec097fc863dda245e686c4
deandrawhitby <104539455+deandrawhitby@users.noreply.github.com>
2023-01-30 14:47:37 -0600

    Added period_4.py

---
+++ period_4.py
@@ -0,0 +1,38 @@
+# define a function
+def pizza_price(toppings):
+    
+    # variable assignment
+    # equal sign (=) - assignment
+    base_price = 15
+    
+    for letter in toppings:
+    
+        # two equal signs (==) - equvilancy
+        if letter == "T":
+            base_price = base_price + 1.5
+            
+        if letter == "O":
+            base_price = base_price + 1.25
+            
+        if letter == "P":
+            base_price = base_price + 3.50
+         
+        if letter == "M":
+            base_price = base_price = 3.75
+            
+        if letter == "A":
+            base_price = base_price = 0.40
+            
+        return base_price
+            
+
+        
+        
+        
+        
+        
+        
+        
+if __name__ == "__main__":
+    price = pizza_price("TPM")
+    print(price)
