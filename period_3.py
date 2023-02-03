4a0a3d0cc54fbc27008013cd44393bb8d71220c2
deandrawhitby <104539455+deandrawhitby@users.noreply.github.com>
2023-02-03 12:42:49 -0600

    Updated period_3.py

--- period_3.py
+++ period_3.py
@@ -1,12 +1,19 @@
 # define a function
 def pizza_price(toppings):
     
+    toppings = toppings.upper()
+    print(toppings)
+    
     base_price = 15
+    calculated_toppings_list = []
     
     # use "for" loop to iterate through toppings
-    
     for item in toppings:
         
+        if item in calculated_toppings_list:
+            continue
+        
+
         if item == "T":
             base_price = base_price + 1.5
             
@@ -19,15 +26,21 @@ def pizza_price(toppings):
         if item == "M":
             base_price = base_price + 3.75
             
-        if item == "a":
+        if item == "A":
             base_price = base_price + .40
             
+        calculated_toppings_list.append(item)
+            
+    # Apply 5% if more than $20
     if base_price > 20:
-        ...
+        base_price = base_price * .95
+        
+        
+    base_price = round(base_price, 2)
 
     print(base_price)
     
-    
+        
     
     
     
